e3523830377b24bd3cf9e5a8d534147b1a30fa24
Alex Aloia <alexander.aloia@theice.com>
2017-09-09 16:10:38 -0500

    swap out webpack-dev-server for express -- nested urls were not being server

---
+++ server.js
@@ -0,0 +1,46 @@
+const path                 = require('path'),
+      express              = require('express'),
+      webpack              = require('webpack'),
+      webpackMiddleware    = require('webpack-dev-middleware'),
+      webpackHotMiddleware = require('webpack-hot-middleware'),
+      config               = require('./webpack.config.js'),
+
+      isDeveloping         = process.env.NODE_ENV !== 'production',
+      port                 = isDeveloping ? 3000 : process.env.PORT,
+      app                  = express();
+
+if (isDeveloping) {
+    compiler         = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+        publicPath : config.output.publicPath,
+        contentBase: 'src',
+        stats      : {
+            colors      : true,
+            hash        : false,
+            timings     : true,
+            chunks      : false,
+            chunkModules: false,
+            modules     : false
+        }
+    });
+
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.get('*', function response(req, res) {
+        res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+        res.end();
+    });
+} else {
+    app.use(express.static(__dirname + '/dist'));
+    app.get('*', function response(req, res) {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
+}
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,69 @@
-const path = require('path'),
-    webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+'use strict';
 
-const src = path.join(__dirname, 'src'),
-    dist = path.join(__dirname, 'dist');
+const path              = require('path'),
+      webpack           = require('webpack'),
+      HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src  = path.join(__dirname, 'src'),
+      dist = path.join(__dirname, 'dist');
+
+// TODO: resolve vs join
+// path      : path.resolve(__dirname, 'dist'),
 
 module.exports = {
-    // entry: {
-    // app: './src/index.js'
-    // main: './src/index.js'
-    // },
-    entry: './src/index.js',
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+        `${src}/index.js`
+    ],
 
-    // devtool: 'inline-source-map',
-    // devtool: 'source-map',
-    devServer: {
-        // publicPath: "/dist/"
-        // only needed if serving static content, publicPath takes precedence???
-        // contentBase: path.join(__dirname, './dist')
+    output: {
+        path      : dist,
+        filename  : '[name].js',
+        publicPath: '/'
     },
+
+    plugins: [
+        //     new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title   : 'Development',
+            filename: 'index.html',
+            template: `${src}/index.hbs`,
+            inject: 'body',
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test   : /\.js$/,
                 exclude: /node_modules/,
-                loaders: ['babel-loader'],
-                include: path.join(__dirname, 'src')
+                loaders: ['babel-loader']
+            }, {
+                test  : /\.json?$/,
+                loader: 'json'
             },
             {
                 test: /\.scss$/,
-                use: [
+                use : [
                     {
-                        loader: "style-loader" // creates style nodes from JS strings
+                        loader: 'style-loader' // creates style nodes from JS strings
                     },
                     {
-                        loader: "css-loader" // translates CSS into CommonJS
+                        loader: 'css-loader' // translates CSS into CommonJS
                     },
                     {
-                        loader: "sass-loader", // compiles Sass to CSS
+                        loader : 'sass-loader', // compiles Sass to CSS
                         options: {
-                            includePaths: ['./node_modules', './node_modules/grommet/node_modules']
+                            includePaths: ['./node_modules']
                         }
                     }]
-            },
-            // {
-            //     test: /(foundtion\.core)/,
-            //     use: [
-            //         {
-            //             loader: 'exports?foundation=jQuery.fn.foundation'
-            //         }
-            //     ]
-            //
-            // }
+            }
         ]
-    },
-    plugins: [
-        // new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            // filename: './dist/index.html'
-            template: `${src}/index.hbs`
-
-        }),
-
-        // new webpack.ProvidePlugin({
-        //     $: 'jquery',
-        //     jQuery: 'jquery',
-        //     'window.jQuery': 'jquery'
-        // })
-    ],
-    // resolve: {
-    //     extensions: ['.js', '.jsx'],
-    //     alias: {
-    //         foundation: 'foundation-sites/dist/js/foundation.js'
-    //     }
-    // },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
     }
 };
